a74b9ecfc10c060d1c66414a92038f7c95a62953
Yurii Shatskyi <yurii.shatskyi.it@gmail.com>
2022-11-10 17:23:29 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+stopBtn.disabled = true;
+
+function getRandomHexColor() {
+    return document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;        
+};
+
+startBtn.addEventListener('click', onStartClick); 
+
+function onStartClick() {
+    timerId = setInterval(() => {
+        getRandomHexColor();
+    }, 1000);
+
+    startBtn.disabled = true;
+    stopBtn.disabled = false;  
+};
+
+stopBtn.addEventListener('click', onStopClick);
+
+function onStopClick() {
+    clearInterval(timerId);
+    
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+};
 
