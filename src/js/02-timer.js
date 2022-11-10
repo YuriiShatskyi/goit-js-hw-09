f4a0af2336aaef6c9c6a9a00892904de4273ccf0
Yurii Shatskyi <yurii.shatskyi.it@gmail.com>
2022-11-10 23:12:37 +0200

    task 2 done

---
+++ src/js/02-timer.js
@@ -0,0 +1,81 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.5.min.css";
+
+
+const refs = {
+  date: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+}
+    
+refs.startBtn.disabled = true;
+let timerId = null;
+let ms = 0;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+
+    if (selectedDates[0] < Date.now()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      return;
+    };
+    refs.startBtn.disabled = false;
+
+    refs.startBtn.addEventListener('click', () => {
+      Notiflix.Notify.success('Timer started');
+      timerId = setInterval(() => {
+        ms = selectedDates[0] - Date.now();
+        const updateTimerObj = convertMs(ms);
+        
+        updateTimer(updateTimerObj);
+
+        if (ms <= 1000) {
+          clearInterval(timerId);
+          Notiflix.Notify.success('Timer stoped');
+        }
+      }, 1000);
+    });
+  },
+};
+
+flatpickr(refs.date, options);
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+};
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, 0);
+};
+
+function updateTimer(updateTimerObj) {
+  refs.seconds.textContent = addLeadingZero(updateTimerObj.seconds);
+  refs.minutes.textContent = addLeadingZero(updateTimerObj.minutes);
+  refs.hours.textContent = addLeadingZero(updateTimerObj.hours);
+  refs.days.textContent = addLeadingZero(updateTimerObj.days);
+};
